cba060d09a94b5db02a563afaa46db1a721a1b02
shrutiramenahalli <shrutir456@gmail.com>
2020-11-19 19:44:36 +0000

    Update classify.py

--- Raspi-model/classify.py
+++ Raspi-model/classify.py
@@ -74,22 +74,17 @@ def main():
     #                      metrics=['accuracy'])
 
     model = tflite.Interpreter(model_path=args.model_name)
-    
+    for x in os.listdir(args.captcha_dir):
+        # load image and preprocess it
+        raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
+        rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
+        image = numpy.array(rgb_data) / 255.0
+        (c, h, w) = image.shape
+        image = image.reshape([-1, c, h, w])
+        prediction = model.predict(image)
+        output_file.write(x + ", " + decode(captcha_symbols, prediction) + "\n")
 
-            for x in os.listdir(args.captcha_dir):
-                # load image and preprocess it
-                raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
-                rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
-                image = numpy.array(rgb_data) / 255.0
-                (c, h, w) = image.shape
-                image = image.reshape([-1, c, h, w])
-                prediction = model.predict(image)
-                output_file.write(x + ", " + decode(captcha_symbols, prediction) + "\n")
-
-                print('Classified ' + x)
-
-
-           
+        print('Classified ' + x)
 
 if __name__ == '__main__':
     main()
