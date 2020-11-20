55e1e2bef688967180db6bed6358858a74f59c5a
shrutiramenahalli <shrutir456@gmail.com>
2020-11-20 11:28:34 +0000

    Add files via upload

---
+++ Raspi-model/classify.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import os
+import cv2
+import numpy
+import string
+import random
+import argparse
+import tensorflow as tf
+#import tensorflow.keras as keras
+
+import tflite_runtime.interpreter as tflite
+from PIL import Image
+
+#TF_LITE_MODEL_FILENAME = "tf_lite_model.tflite."
+
+
+
+def decode(characters, y):
+    y = numpy.argmax(numpy.array(y), axis=1)
+    #print("Characters are: {0}, y is: {1}".format(characters, y))
+    return ''.join([characters[x] for x in y])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model-name', help='Model name to use for classification', type=str)
+    parser.add_argument('--captcha-dir', help='Where to read the captchas to break', type=str)
+    parser.add_argument('--output', help='File where the classifications should be saved', type=str)
+    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
+    args = parser.parse_args()
+
+    if args.model_name is None:
+        #args.model_name = 'Captcha_Solver'
+        print("Please specify the CNN model to use")
+        exit(1)
+
+    if args.captcha_dir is None:
+        #args.captcha_dir = '/users/pgrad/ramenahs/PycharmProjects/ScalableComputing/sample-code/Raspi/RAMENAHS-project2rpi'
+        print("Please specify the directory with captchas to break")
+        exit(1)
+
+    if args.output is None:
+        #args.output = '/users/pgrad/ramenahs/PycharmProjects/ScalableComputing/sample-code/Raspi/results1.txt'
+        print("Please specify the path to the output file")
+        exit(1)
+
+    if args.symbols is None:
+        #args.symbols =  '/users/pgrad/ramenahs/PycharmProjects/ScalableComputing/sample-code/Raspi/symbolsRasp.txt'
+        print("Please specify the captcha symbols file")
+        exit(1)
+
+    symbols_file = open(args.symbols, 'r')
+    captcha_symbols = symbols_file.readline().strip()
+    symbols_file.close()
+
+    print("Classifying captchas with symbol set {" + captcha_symbols + "}")
+
+    #with tf.device('/cpu:0'):
+    #    with open(args.output, 'w') as output_file:
+    #        json_file = open(args.model_name+'.json', 'r')
+    #        loaded_model_json = json_file.read()
+    #        json_file.close()
+    #        model = keras.models.model_from_json(loaded_model_json)
+    #        model.load_weights(args.model_name+'.h5')
+    #        model.compile(loss='categorical_crossentropy',
+    #                      optimizer=keras.optimizers.Adam(1e-3, amsgrad=True),
+    #                      metrics=['accuracy'])
+
+
+    interpreter = tflite.Interpreter(model_path=args.model_name)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+        
+    with open(args.output, 'w') as output_file:
+    
+        for x in os.listdir(args.captcha_dir):
+            raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
+            rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
+            image = numpy.array(rgb_data, dtype=numpy.float32) / 255.0
+            (c, h, w) = image.shape
+            image = image.reshape([-1, c, h, w])
+            interpreter.set_tensor(input_details[0]['index'], image)
+            interpreter.invoke()
+            
+            prediction = []
+
+            for result in output_details:
+                output_data = interpreter.get_tensor(result['index'])
+                prediction.append(output_data)
+
+            
+            prediction = numpy.reshape(prediction, (len(output_details), -1))
+        
+            output_file.write(x + "," + decode(captcha_symbols, prediction) + "\n")
+
+            print('Classified ' + x)
+
+
+if __name__ == '__main__':
+    main()
